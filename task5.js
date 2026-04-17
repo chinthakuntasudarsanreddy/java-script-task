32bd0ec58f0623151c74e1dc7cf63a8b7a23fe55
chinthakuntasudarsanreddy <139463795+chinthakuntasudarsanreddy@users.noreply.github.com>
2026-04-17 20:55:50 +0530

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,3 @@
+console.log(typeof "JavaScript"); // string
+console.log(typeof 250);          // number
+console.log(typeof false);        // boolean
---
+++ task6.js
@@ -0,0 +1,4 @@
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+console.log(fruits);                     // full array
+console.log(fruits[0]);                  // first fruit
+console.log(fruits.at(-1));  // last fruit
