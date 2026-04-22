119a79bfaf08c0db38b58d5340370d47cd065fec
chinthakuntasudarsanreddy <139463795+chinthakuntasudarsanreddy@users.noreply.github.com>
2026-04-22 16:18:13 +0530

    Delete task6.js

--- task6.js
+++
@@ -1,4 +0,0 @@
-let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
-console.log(fruits);                     // full array
-console.log(fruits[0]);                  // first fruit
-console.log(fruits.at(-1));  // last fruit
